11049a3ea13348b8813a3fb0f2eef0f248aff41c
PLakshmiAlekya <47251464+PLakshmiAlekya@users.noreply.github.com>
2021-05-24 12:58:11 +0530

    Update Admission.java

--- OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Admission.java
+++ OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Admission.java
@@ -9,5 +9,48 @@ public class Admission
 	private Program program;
 	private Course course;
 	private String year;
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getApplicationId() {
+		return applicationId;
+	}
+	public void setApplicationId(String applicationId) {
+		this.applicationId = applicationId;
+	}
+	public String getAdmissionStatus() {
+		return admissionStatus;
+	}
+	public void setAdmissionStatus(String admissionStatus) {
+		this.admissionStatus = admissionStatus;
+	}
+	public College getCollege() {
+		return college;
+	}
+	public void setCollege(College college) {
+		this.college = college;
+	}
+	public Program getProgram() {
+		return program;
+	}
+	public void setProgram(Program program) {
+		this.program = program;
+	}
+	public Course getCourse() {
+		return course;
+	}
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	
 
 }
