8a2152b98eb0e478fa4e0e46d5adc644d925adc7
PLakshmiAlekya <47251464+PLakshmiAlekya@users.noreply.github.com>
2021-05-24 12:58:58 +0530

    Update Branch.java

--- OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Branch.java
+++ OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Branch.java
@@ -5,5 +5,23 @@ public class Branch
 	private int branchId;
 	private String branchName;
 	private String branchDescription;
+	public int getBranchId() {
+		return branchId;
+	}
+	public void setBranchId(int branchId) {
+		this.branchId = branchId;
+	}
+	public String getBranchName() {
+		return branchName;
+	}
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+	public String getBranchDescription() {
+		return branchDescription;
+	}
+	public void setBranchDescription(String branchDescription) {
+		this.branchDescription = branchDescription;
+	}
 	
 }
