2ce3a0da526e84c5a0ab785a6527f15b8e19bcff
PLakshmiAlekya <47251464+PLakshmiAlekya@users.noreply.github.com>
2021-05-25 10:57:19 +0530

    Update Admission.java

--- OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Admission.java
+++ OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Admission.java
@@ -1,14 +1,45 @@
 package com.cg.oms.entity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+@Entity
+@Table(name = "admission")
 
 public class Admission
 {
+	@Column(name = "email_id")
 	private String emailId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "application_id")
 	private String applicationId;
+	@Column(name = "admission_status")
 	private String admissionStatus;//Confirm after payement done;
+	@Column(name = "college")
 	private College college;
+	@Column(name = "program")
 	private Program program;
+	@Column(name = "course")
 	private Course course;
+	@Column(name = "year")
 	private String year;
+	public Admission() {
+		
+	}
+	public Admission(String emailId, String applicationId, String admissionStatus, College college, Program program,
+			Course course, String year) {
+		super();
+		this.emailId = emailId;
+		this.applicationId = applicationId;
+		this.admissionStatus = admissionStatus;
+		this.college = college;
+		this.program = program;
+		this.course = course;
+		this.year = year;
+	}
 	public String getEmailId() {
 		return emailId;
 	}
