c3be580576d75d5fe4a1bf37a666d2368a3a4ce7
meghana257 <84558386+meghana257@users.noreply.github.com>
2021-05-28 16:09:52 +0530

    Update Address.java

--- OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Address.java
+++ OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Address.java
@@ -1,12 +1,95 @@
 package com.cg.oms.entity;
 
-public class Address 
-{
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int addressId;
 	private String city;
 	private String district;
-	private  String state;
+	private String state;
 	private String country;
 	private String zipcode;
 	private String landmark;
+
+	public Address() {
+
+	}
+
+	public Address( String city, String district, String state, String country, String zipcode,
+			String landmark) {
+		super();
+		
+		this.city = city;
+		this.district = district;
+		this.state = state;
+		this.country = country;
+		this.zipcode = zipcode;
+		this.landmark = landmark;
+	}
+
+	public int getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getLandmark() {
+		return landmark;
+	}
+
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+
 }
