4dc096d462e58beb1abddfda8bfb73856c2ea04e
meghana257 <84558386+meghana257@users.noreply.github.com>
2021-05-28 16:20:13 +0530

    Update Admission.java

--- OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Admission.java
+++ OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Admission.java
@@ -1,87 +1,107 @@
 package com.cg.oms.entity;
-import javax.persistence.Column;
+
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-@Entity
-@Table(name = "admission")
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
-public class Admission
-{
-	@Column(name = "email_id")
+@Entity
+public class Admission {
+
+	
 	private String emailId;
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "application_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String applicationId;
-	@Column(name = "admission_status")
+	
 	private String admissionStatus;//Confirm after payement done;
-	@Column(name = "college")
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "collegeRegId")
 	private College college;
-	@Column(name = "program")
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "programId")
 	private Program program;
-	@Column(name = "course")
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "courseId")
 	private Course course;
-	@Column(name = "year")
 	private String year;
-	public Admission() {
-		
-	}
-	public Admission(String emailId, String applicationId, String admissionStatus, College college, Program program,
+	
+	public Admission(String emailId, String admissionStatus, College college, Program program,
 			Course course, String year) {
-		super();
 		this.emailId = emailId;
-		this.applicationId = applicationId;
+	
 		this.admissionStatus = admissionStatus;
 		this.college = college;
 		this.program = program;
 		this.course = course;
 		this.year = year;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
 	public String getApplicationId() {
 		return applicationId;
 	}
+
 	public void setApplicationId(String applicationId) {
 		this.applicationId = applicationId;
 	}
+
 	public String getAdmissionStatus() {
 		return admissionStatus;
 	}
+
 	public void setAdmissionStatus(String admissionStatus) {
 		this.admissionStatus = admissionStatus;
 	}
+
 	public College getCollege() {
 		return college;
 	}
+
 	public void setCollege(College college) {
 		this.college = college;
 	}
+
 	public Program getProgram() {
 		return program;
 	}
+
 	public void setProgram(Program program) {
 		this.program = program;
 	}
+
 	public Course getCourse() {
 		return course;
 	}
+
 	public void setCourse(Course course) {
 		this.course = course;
 	}
+
 	public String getYear() {
 		return year;
 	}
+
 	public void setYear(String year) {
 		this.year = year;
 	}
-	
 
+	@Override
+	public String toString() {
+		return "Admission [emailId=" + emailId + ", applicationId=" + applicationId + ", admissionStatus="
+				+ admissionStatus + ", college=" + college + ", program=" + program + ", course=" + course + ", year="
+				+ year + "]";
+	}
+	
 }
