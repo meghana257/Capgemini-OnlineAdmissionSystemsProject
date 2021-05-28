a9ddf78c4d6c6aa442688622634a2b05479bd491
meghana257 <84558386+meghana257@users.noreply.github.com>
2021-05-28 16:31:11 +0530

    Update User.java

--- OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/User.java
+++ OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/User.java
@@ -1,7 +1,14 @@
 package com.cg.oms.entity;
 
-public class User
-{
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class User{
+	@Id		
+	
 	private String userId;
 	private String firstName;
 	private String middleName;
@@ -9,6 +16,71 @@ public class User
 	private String email;
 	private String mobileNumber;
 	private String aadharCardNo;
+	
+	
+	public User() {
+		
+	}
+	
+	public User(String userId, String firstName, String middleName, String lastName, String email, String mobileNumber,
+			String aadharCardNo) {
+		super();
+		this.userId = userId;
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.email = email;
+		this.mobileNumber = mobileNumber;
+		this.aadharCardNo = aadharCardNo;
+		
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public String getAadharCardNo() {
+		return aadharCardNo;
+	}
+	public void setAadharCardNo(String aadharCardNo) {
+		this.aadharCardNo = aadharCardNo;
+	}
+	
+
+	
+	
 	
 	
 }
