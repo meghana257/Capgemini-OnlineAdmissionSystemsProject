c267826a9ade7405a7c0a62433888d030b7e0ec6
meghana257 <84558386+meghana257@users.noreply.github.com>
2021-05-28 16:24:36 +0530

    Update Document.java

--- OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Document.java
+++ OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Document.java
@@ -1,11 +1,68 @@
 package com.cg.oms.entity;
 
-public class Document
-{
-private int documentId;
-private String documentName;
-private String documentUrl;
-private int applicantId;
-private String emailId;
-private String documentStatus;//Uploaded/NotUploaded
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Document {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int documentId;
+	private String documentName;
+	private String documentUrl;
+	private int applicantId;
+	private String emailId;
+	private String documentStatus;// Uploaded/NotUploaded
+	
+	
+	public Document(int documentId, String documentName, String documentUrl, int applicantId, String emailId,
+			String documentStatus) {
+		super();
+		this.documentId = documentId;
+		this.documentName = documentName;
+		this.documentUrl = documentUrl;
+		this.applicantId = applicantId;
+		this.emailId = emailId;
+		this.documentStatus = documentStatus;
+	}
+	public int getDocumentId() {
+		return documentId;
+	}
+	public void setDocumentId(int documentId) {
+		this.documentId = documentId;
+	}
+	public String getDocumentName() {
+		return documentName;
+	}
+	public void setDocumentName(String documentName) {
+		this.documentName = documentName;
+	}
+	public String getDocumentUrl() {
+		return documentUrl;
+	}
+	public void setDocumentUrl(String documentUrl) {
+		this.documentUrl = documentUrl;
+	}
+	public int getApplicantId() {
+		return applicantId;
+	}
+	public void setApplicantId(int applicantId) {
+		this.applicantId = applicantId;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getDocumentStatus() {
+		return documentStatus;
+	}
+	public void setDocumentStatus(String documentStatus) {
+		this.documentStatus = documentStatus;
+	}
+	
+	
 }
