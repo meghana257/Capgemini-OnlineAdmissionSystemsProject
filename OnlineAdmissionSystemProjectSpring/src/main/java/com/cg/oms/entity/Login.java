46b17ebd7865707a9ed145f6b41bc90eafd826b7
meghana257 <84558386+meghana257@users.noreply.github.com>
2021-05-28 16:25:19 +0530

    Update Login.java

--- OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Login.java
+++ OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/Login.java
@@ -1,9 +1,50 @@
 package com.cg.oms.entity;
 
-public class Login 
-{
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Login {
+	
+	@Id
+	
 	private String userId;
 	private String password;
 	private String role;
+
+	public Login() {
+
+	}
+
+	public Login( String password, String role) {
+		super();
+		this.password = password;
+		this.role = role;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 
 }
