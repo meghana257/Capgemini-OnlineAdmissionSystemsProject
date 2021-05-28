8e995aeda3f452ea4d1b2f3951807ae13f3b44ec
meghana257 <84558386+meghana257@users.noreply.github.com>
2021-05-28 16:28:41 +0530

    Update University.java

--- OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/University.java
+++ OnlineAdmissionSystemProjectSpring/src/main/java/com/cg/oms/entity/University.java
@@ -1,57 +1,79 @@
 package com.cg.oms.entity;
+import java.util.List;
 
-import java.util.ArrayList;
+import javax.persistence.CascadeType;
 
-public class University 
-{
-	private String name;
-	private int universityId;
-	private Address address;
-	private ArrayList<College> collgeList;
-	
-	public University()
-	{
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+
+@Entity
+public class University{
 		
+	private String name;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int universityId;
+	
+	@Embedded
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="UniversityAddress_Id",referencedColumnName="addressId")
+	private Address address;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name="universityId")
+	private List<College> collgeList;
+	
+	public University() {
+	
 	}
 	
-	public University(Address address, String name, int universityId, ArrayList<College> collgeList) {
+	public University(String name, Address address,List<College> collgeList) {
 		super();
-		this.address = address;
 		this.name = name;
-		this.universityId = universityId;
+		this.address = address;
 		this.collgeList = collgeList;
 	}
 
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public int getUniversityId() {
 		return universityId;
 	}
-	
+
 	public void setUniversityId(int universityId) {
 		this.universityId = universityId;
 	}
-	
+
 	public Address getAddress() {
 		return address;
 	}
-	
+
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-	
-	public ArrayList<College> getCollgeList() {
+
+	public List<College> getCollgeList() {
 		return collgeList;
 	}
-	
-	public void setCollgeList(ArrayList<College> collgeList) {
+
+	public void setCollgeList(List<College> collgeList) {
 		this.collgeList = collgeList;
 	}
 	
+	
+
+
 }
